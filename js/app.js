f4e01fe2605095964cd26ab66b373f5f1dca5c7a
solangealmn <solangealmn@gmail.com>
2018-07-14 19:55:42 -0300

    Sistema de abas implementado

--- js/app.js
+++ js/app.js
@@ -3,11 +3,11 @@
  */
  //Funcionalidade do menu hamburguer
  function openNav() {
-     document.getElementById("mySidenav").style.width = "250px";
+     document.getElementById("sideNav").style.width = "250px";
  }
 
  function closeNav() {
-     document.getElementById("mySidenav").style.width = "0";
+     document.getElementById("sideNav").style.width = "0"; 
  }
 // Puedes hacer uso de la base de datos a través de la variable `data`
 console.log(data);
