edf1751fa05c1e35419a477bcfc0c0881cd9bcdc
solangealmn <solangealmn@gmail.com>
2018-07-14 00:40:22 -0300

    Início da página de overview

--- js/app.js
+++ js/app.js
@@ -1,6 +1,13 @@
 /*
  * Funcionalidad de tu producto
  */
+ //Funcionalidade do menu hamburguer
+ function openNav() {
+     document.getElementById("mySidenav").style.width = "250px";
+ }
 
+ function closeNav() {
+     document.getElementById("mySidenav").style.width = "0";
+ }
 // Puedes hacer uso de la base de datos a través de la variable `data`
 console.log(data);
