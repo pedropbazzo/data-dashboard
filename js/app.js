7bc5644fa21a0c33f5e410c2ca4ce02a83c5082c
J. Simplicio <jessicasimplicio1@gmail.com>
2018-07-15 04:58:52 -0300

    implementacao js de seletores de sedes e geracao

--- js/app.js
+++ js/app.js
@@ -3,11 +3,22 @@
  */
  //Funcionalidade do menu hamburguer
  function openNav() {
-     document.getElementById("sideNav").style.width = "250px";
+ 	document.getElementById("sideNav").style.width = "250px";
  }
 
  function closeNav() {
-     document.getElementById("sideNav").style.width = "0"; 
+  document.getElementById("sideNav").style.width = "0"; 
  }
+
+function filter() {
+	var headOptions = document.getElementById( "selectHeads");
+	var headSelected = headOptions.options[headOptions.selectedIndex].value;
+	
+	var generationOptions = document.getElementById( "selectGeneration");
+	var generationSelected = generationOptions.options[generationOptions.selectedIndex].value;
+
+	//alert(headSelected + generationSelected);
+}
+
 // Puedes hacer uso de la base de datos a través de la variable `data`
-console.log(data);
+//console.log(data);
